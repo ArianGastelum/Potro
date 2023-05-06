f0516b1b714d971836fbf385e4e9993a8e19d31d
Arian Gastelum <Arian Gastelum@PcArian>
2023-05-05 21:40:49 -0700

    Creación del proyecto

---
+++ src/mx/itson/potro/entidades/Alumno.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.itson.potro.entidades;
+
+/**
+ *
+ * @author Arian Gastelum
+ */
+public class Alumno {
+    private int id;
+    private String nombre;
+    
+}
